e4ed5300394ad300b0dfb9c9bade0bd18e76e6f7
Sir-Dazzling <ottidan20@gmail.com>
2021-01-20 08:15:35 +0100

    finished basic stuffs

---
+++ src/data/Stats.ts
@@ -0,0 +1,13 @@
+export const Data = [{
+    id: 1,
+    title: "CORONAVIRUS CASES",
+    number: "1 838 456"
+}, {
+    id: 2,
+    title: "TOTAL DEATHS",
+    number: "1 29 863"
+}, {
+    id: 3,
+    title: "RECOVERED",
+    number: "838 456"
+}]
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,13 @@ export type RootStackParamList = {
     Home: undefined;
     Detail: undefined;
 };
+
+export type StatsData = [{
+    id: number;
+    title: String;
+    number: String;
+}]
+
+export type Stats = {
+    data: StatsData[]
+}
